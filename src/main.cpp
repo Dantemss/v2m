428904d3bfdd14bf71255fd6ae19386c8e44ae52
Dante Soares <dante.m.soares@rice.edu>
2015-10-13 00:12:59 -0500

    Main now receives the video filename or device as argument

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,22 @@
 
 using namespace parallel_cv;
 
-#define VIDEO_PATH "videos/video.mpeg"
 #define NUM_WORKER_THREADS 8
 #define COMMAND_FUNCTION command::denseFlow
 
-int main(void) {
-  run(VIDEO_PATH, NUM_WORKER_THREADS, COMMAND_FUNCTION);
+int main(int argc, char *argv[]) {
+  cv::String video_path;
+
+  switch(argc) {
+    case 1: video_path = "0";
+            break;
+    case 2: video_path = argv[1];
+            break;
+    default: log("Error: too many arguments");
+             return 1;
+  }
+
+  run(video_path, NUM_WORKER_THREADS, COMMAND_FUNCTION);
 
   return 0;
 }
