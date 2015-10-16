b3c3c6b9221e587433f46a8a2eb6111d41a49f9c
Dante Soares <dante.m.soares@rice.edu>
2015-10-15 20:12:38 -0500

    Map now estimates the Z velocity and curl around several axis for each point

--- src/parallel_cv/commands/map.cpp
+++ src/parallel_cv/commands/map.cpp
@@ -5,39 +5,30 @@
 
 using namespace cv;
 
+#define MAX_FLOW 25
+#define K_SCALE 0.25*(2 - sqrt(2))
+#define QRTR_SQRT_2_SCALED 0.125*(sqrt(2) - 1)
+
 namespace parallel_cv {
   namespace commands {
-    inline bool isFlowCorrect(Point2f u) {
-      return !cvIsNaN(u.x) && !cvIsNaN(u.y) && fabs(u.x) < 1e9 && fabs(u.y) < 1e9;
-    }
+    Matx33d div_vx_kernel(QRTR_SQRT_2_SCALED, 0, -QRTR_SQRT_2_SCALED,
+                          K_SCALE,            0, -K_SCALE,
+                          QRTR_SQRT_2_SCALED, 0, -QRTR_SQRT_2_SCALED);
+    Matx33d div_vy_kernel(QRTR_SQRT_2_SCALED,  K_SCALE,  QRTR_SQRT_2_SCALED,
+                          0,                   0,        0,
+                          -QRTR_SQRT_2_SCALED, -K_SCALE, -QRTR_SQRT_2_SCALED);
 
-    static void drawOpticalFlow(const Mat_<Point2f>& flow, Mat& dst, float maxmotion = -1) {
-      dst = Mat::zeros(flow.size(), CV_8UC1);
+    Matx33d curlz_vx_kernel(QRTR_SQRT_2_SCALED,  K_SCALE,  QRTR_SQRT_2_SCALED,
+                            0,                   0,        0,
+                            -QRTR_SQRT_2_SCALED, -K_SCALE, -QRTR_SQRT_2_SCALED);
+    Matx33d curlz_vy_kernel(-QRTR_SQRT_2_SCALED, 0, QRTR_SQRT_2_SCALED,
+                            -K_SCALE,            0, K_SCALE, 
+                            -QRTR_SQRT_2_SCALED, 0, QRTR_SQRT_2_SCALED);
 
-      // determine motion range:
-      float maxrad = maxmotion;
-
-      if (maxmotion <= 0) {
-        maxrad = 1;
-        for (int y = 0; y < flow.rows; ++y) {
-          for (int x = 0; x < flow.cols; ++x) {
-            Point2f u = flow(y, x);
-
-            if (!isFlowCorrect(u)) continue;
-
-            maxrad = max(maxrad, sqrt(u.x * u.x + u.y * u.y));
-          }
-        }
-      }
-
-      for (int y = 0; y < flow.rows; ++y) {
-        for (int x = 0; x < flow.cols; ++x) {
-          Point2f u = flow(y, x);
-
-          if (isFlowCorrect(u) && (u.x > 0.1 || u.y > 0.1)) dst.at<uchar>(y, x) = 1;
-        }
-      }
-    }
+    Matx13d curly_vz_kernel(0.5, 0, -0.5);
+    Matx31d curlx_vz_kernel(-0.5,
+                            0,
+                            0.5);
 
     /**
      * @function map
@@ -51,15 +42,36 @@ namespace parallel_cv {
 
       Mat_<Point2f> flow;
       calcOpticalFlowFarneback(prev_gray, gray, flow, 0.5, 3, 20, 3, 5, 1.1, 0);
-      //Ptr<DualTVL1OpticalFlow> tvl1 = createOptFlow_DualTVL1();
-      //tvl1->calc(prev_gray, gray, flow);
 
-      Mat flow_out, out;
-      drawOpticalFlow(flow, flow_out);
-      out = Scalar::all(0);
-      frame.copyTo(out, flow_out);
+      int i, j;
+      Point2f p;
+      Size flow_size = flow.size();
+      Mat_<double> vx(flow_size), vy(flow_size), vz(flow_size),
+                   curlx(flow_size), curly(flow_size), curlz(flow_size),
+                   temp_x(flow_size), temp_y(flow_size);
+      for (i = 0; i < flow_size.height; i++) {
+        for (j = 0; j < flow_size.width; j++) {
+          p = flow(i, j);
 
-      return out;
+          if (cvIsNaN(p.x) || cvIsNaN(p.y) || abs(p.x) > MAX_FLOW || abs(p.y) > MAX_FLOW) continue;
+
+          vx(i, j)  = p.x;
+          vy(i, j)  = p.y;
+        }
+      }
+
+      filter2D(vx, temp_x, -1, div_vx_kernel);
+      filter2D(vy, temp_y, -1, div_vy_kernel);
+      vz = temp_x + temp_y;
+
+      filter2D(vx, temp_x, -1, curlz_vx_kernel);
+      filter2D(vy, temp_y, -1, curlz_vy_kernel);
+      curlz = temp_x + temp_y;
+
+      filter2D(vz, curly, -1, curly_vz_kernel);
+      filter2D(vz, curlx, -1, curlx_vz_kernel);
+
+      return vz + 0.5;
     }
   }
 }
