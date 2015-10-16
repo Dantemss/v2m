8d18f02c1365f44044a25c9e0d5a012180958f60
Dante Soares <dante.m.soares@rice.edu>
2015-10-16 01:04:37 -0500

    Extracted TVL1 params into #defines

--- src/parallel_cv/commands/map.cpp
+++ src/parallel_cv/commands/map.cpp
@@ -10,13 +10,28 @@ using namespace cv;
 #define K_SCALE            0.25*(2 - sqrt(2))
 #define QRTR_SQRT_2_SCALED 0.125*(sqrt(2) - 1)
 
-#define PYR_SCALE  0.5
-#define LEVELS     3
-#define WINSIZE    20
-#define ITERATIONS 3
-#define POLY_N     5
-#define POLY_SIGMA 1.1
-#define FLAGS      0
+#define TVL1
+
+#define FB_PYR_SCALE  0.5
+#define FB_LEVELS     3
+#define FB_WINSIZE    20
+#define FB_ITERATIONS 3
+#define FB_POLY_N     7
+#define FB_POLY_SIGMA 1.5
+#define FB_FLAGS      OPTFLOW_FARNEBACK_GAUSSIAN
+
+#define TVL1_TAU              0.25
+#define TVL1_LAMBDA           0.15
+#define TVL1_THETA            0.3
+#define TVL1_GAMMA            0.0
+#define TVL1_SCALES_NUMBER    1
+#define TVL1_WARPINGS_NUMBER  1
+#define TVL1_EPSILON          0.1
+#define TVL1_INNER_ITERATIONS 30
+#define TVL1_OUTER_ITERATIONS 10
+#define TVL1_USE_INITIAL_FLOW false
+#define TVL1_SCALE_STEP       0.8
+#define TVL1_MEDIAN_FILTERING 5
 
 namespace parallel_cv {
   namespace commands {
@@ -50,10 +65,30 @@ namespace parallel_cv {
       cvtColor(prev, prev_gray, CV_BGR2GRAY);
 
       Mat_<Point2f> flow;
-      calcOpticalFlowFarneback(prev_gray, gray, flow,
-                               PYR_SCALE, LEVELS, WINSIZE, ITERATIONS, POLY_N, POLY_SIGMA, FLAGS);
-      //Ptr<DualTVL1OpticalFlow> tvl1 = createOptFlow_DualTVL1();
-      //tvl1->calc(prev_gray, gray, flow);
+      #ifdef FARNEBACK
+        calcOpticalFlowFarneback(
+          prev_gray, gray, flow,
+          FB_PYR_SCALE, FB_LEVELS, FB_WINSIZE, FB_ITERATIONS, FB_POLY_N, FB_POLY_SIGMA, FB_FLAGS
+        );
+      #endif
+
+      #ifdef TVL1
+        Ptr<DualTVL1OpticalFlow> tvl1 = createOptFlow_DualTVL1();
+        tvl1->setTau(TVL1_TAU);
+        tvl1->setLambda(TVL1_LAMBDA);
+        tvl1->setTheta(TVL1_THETA);
+        tvl1->setGamma(TVL1_GAMMA);
+        tvl1->setScalesNumber(TVL1_SCALES_NUMBER);
+        tvl1->setWarpingsNumber(TVL1_WARPINGS_NUMBER);
+        tvl1->setEpsilon(TVL1_EPSILON);
+        tvl1->setInnerIterations(TVL1_INNER_ITERATIONS);
+        tvl1->setOuterIterations(TVL1_OUTER_ITERATIONS);
+        tvl1->setUseInitialFlow(TVL1_USE_INITIAL_FLOW);
+        tvl1->setScaleStep(TVL1_SCALE_STEP);
+        tvl1->setMedianFiltering(TVL1_MEDIAN_FILTERING);
+        tvl1->calc(prev_gray, gray, flow);
+      #endif
+
 
       int i, j;
       Point2f p;
